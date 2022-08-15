f520ccfa9e889fb7a89bd7f73d83a4f3647e662f
Shuvra Saha Rimjim <110848482+shuvra789@users.noreply.github.com>
2022-08-15 22:43:54 +0600

    Add files via upload

---
+++ lab5.c
@@ -0,0 +1,32 @@
+//Write a function to draw a rocket.
+#include <stdio.h>
+
+ void rocket(void);
+
+ int main(void)
+ {
+     rocket();
+     return 0;
+ }
+
+  void rocket(void)
+  {
+      printf("        /\\ \n");
+      printf("       /  \\ \n");
+      printf("      /    \\ \n");
+      printf("     /______\\ \n");
+      printf("    |        | \n");
+      printf("    |        | \n");
+      printf("    | rocket | \n");
+      printf("    | CSE115 | \n");
+      printf("    |        | \n");
+      printf("    |        | \n");
+      printf("   /|   ||   |\\ \n");
+      printf("  / |   ||   | \\ \n");
+      printf(" /  |   ||   |  \\ \n");
+      printf("/___|   ||   |___\\ \n");
+      printf("    |        | \n");
+      printf("     \\      / \n");
+      printf("      ||  || \n");
+  }
+
